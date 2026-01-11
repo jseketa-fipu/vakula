13dd799a1206ed5005985b24f1889c430946ee61
Josip Seketa <jseketa@student.unipu.hr>
2026-01-12 00:34:31 +0100

    Removed env.py - relying on os.environ. __init__ cleanup.

--- vakula_common/__init__.py
+++ vakula_common/__init__.py
@@ -1,6 +1,5 @@
 """Shared helpers for Vakula services."""
 
-from vakula_common.env import get_env_int, get_env_optional_float, get_env_str
 from vakula_common.http import HttpClient, create_session
 from vakula_common.logging import make_logger, setup_logger
 from vakula_common.models import AdjustRequest, ModuleState, StationState
@@ -13,9 +12,6 @@ __all__ = [
     "ModuleState",
     "StationState",
     "create_session",
-    "get_env_int",
-    "get_env_optional_float",
-    "get_env_str",
     "make_logger",
     "module_name",
     "setup_logger",
